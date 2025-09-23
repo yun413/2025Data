d9ead0a72da4e4700bdc67ba05406b478663af65
wangyunting <wangyunting1024@gmail.com>
2025-09-23 21:20:30 +0800

    250923

--- bmi.js
+++ bmi.js
@@ -1,14 +1,25 @@
 const readline = require('readline-sync');
 
-var weight=0;
+/*var weight=0;
 while(weight<10 || weight>120){
     weight=readline.questionInt("Ur weight? (10~120KG)");
 }
 var height=0;
-while(height<60 || height>230){
+while(height<60 || height>230) {
     height=readline.questionInt("Ur height? (60~230CM)");
 }
+var bmi=weight/((height/100)**2);*/
 
-var bmi=weight/((height/100)**2);
+function askInRange(prompt, min, max) {
+    let value;
+    do {
+        value = readline.questionInt(`${prompt} (${min}~${max}): `);
+    }while (value<min || value>max);
+    return value;
+}
+const weight = askInRange("Ur weight" , 10 , 120);
+const height = askInRange("Ur height" , 60 , 230);
+const bmi=weight/((height/100)**2);
+
 
 console.log("Hello ! Your BMI value is " + bmi.toFixed(2));

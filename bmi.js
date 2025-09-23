9210bb231aade7638d1c7b538be60da65fff0a6f
yun413 <wangyunting1024@gmail.com>
2025-09-23 15:30:38 +0800

    250923

--- bmi.js
+++ bmi.js
@@ -1,7 +1,7 @@
-const reading = require('readline-sync');
+const readline = require('readline-sync');
 
-var weight=65;
-var height=160;
+var weight= readline.question("Ur weight? (KG)");
+var height= readline.question("Ur height? (CM)");
 var bmi=weight/((height/100)**2);
 
 console.log("Hello ! Your BMI value is " + bmi);

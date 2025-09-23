9402a926cafc602c521609c439c979807a07f373
wangyunting <wangyunting1024@gmail.com>
2025-09-23 21:02:22 +0800

    250923

--- bmi.js
+++ bmi.js
@@ -1,7 +1,14 @@
 const readline = require('readline-sync');
 
-var weight= readline.questionFloat("Ur weight? (KG)");
-var height= readline.questionInt("Ur height? (CM)");
+var weight=0;
+while(weight<10 || weight>120){
+    weight=readline.questionInt("Ur weight? (10~120KG)");
+}
+var height=0;
+while(height<60 || height>230){
+    height=readline.questionInt("Ur height? (60~230CM)");
+}
+
 var bmi=weight/((height/100)**2);
 
-console.log("Hello ! Your BMI value is " + bmi);
+console.log("Hello ! Your BMI value is " + bmi.toFixed(2));

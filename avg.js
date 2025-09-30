a0bd74ff4ede0f4421317921ee7a3962e64752b6
yun413 <wangyunting1024@gmail.com>
2025-09-30 15:18:29 +0800

    250930

--- avg.js
+++ avg.js
@@ -1,14 +1,31 @@
+//Ans1 c語言
+// function avg(score)
+// {
+//     var sum=0, average=0;
+//     for(var i=0;i<score.length;i++)
+//     {
+//      sum += score[i];  //是把score[i]的值加到sum上
+//     }
+//     if(score.length>0)
+//         average=sum/score.length;
+//     return average;
+// }
+
+//Ans2 c語言+java
 function avg(score)
 {
     var sum=0, average=0;
     for(var i=0;i<score.length;i++)
     {
-     sum += score[i];  
+     sum += score[i];  //是把score[i]的值加到sum上
     }
-    if(score.length>0)
-        average=sum/score.length;
-    return average;
+    return (score.length>0) ? sum/score.length:0;
 }
+
+
+
+
+
 //test
 var Score=[5,8,20,55,70,41,19];
 //var Score=[];

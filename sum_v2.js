2914c3b4221dc6f87f21f1247726144e9b4ced70
yun413 <wangyunting1024@gmail.com>
2025-10-07 14:08:43 +0800

    251007

---
+++ sum_v2.js
@@ -0,0 +1,22 @@
+//calc 1-2+3-4+.....n
+function sum(n){
+    var sum=0;
+    if(n<=0){
+        console.log("error: n must>0");
+        return null;
+    }else{
+        for(var i=1;i<=n;i++){
+            if(i%2==0) //even %除以會有餘數 
+                sum-=i; 
+            else
+                sum+=i; //odd 奇數
+            //(i%2==0) ? sum-=i : sum+=i; *這段可以取代9-12行 範例在avg.js
+        }
+        return sum;
+    }
+
+}
+
+//test
+var n=100; //readline
+console.log("1-2+3-4+...." +n+ "=" + sum2(n));

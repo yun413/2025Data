c62e0f37988762d4c5d51dc955226a531a9b0200
yun413 <wangyunting1024@gmail.com>
2025-10-07 15:29:58 +0800

    251007

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,24 @@
-//費氏數列
+//費氏數列 //很慢
+//A1:
 function fib(n){
     //prevent n<0
     if(n==0||n==1)
         return n;
     else
         return fib(n-1)+fib(n-2);
+}
+
+
+//用迴圈方式寫 //很快
+//A2:
+function fib(n){
+    //prevent n<0
+    var a=0,b=1;
+    for(var i=1;i<=n;i++){
+        a=a+b;
+        b=a-b;
+    }
+    return a;
 }
 
 //test

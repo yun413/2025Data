d0cdee7c56ffc88533cf047a9cd42ae4cb065d57
yun413 <wangyunting1024@gmail.com>
2025-10-14 16:05:58 +0800

    251014

--- 2dArray.js
+++ 2dArray.js
@@ -5,14 +5,24 @@ var Score=[
 ]
 console.log("Score[0]="+Score[0]);
 
+//小型資料庫
+var Student=[
+    {"name":"jhon", "grade":2, "score":[70,80,90,60,80]},
+    {"name":"tom", "grade":2, "score":[80,90,70,75,85]},
+    {"name":"art", "grade":3, "score":[85,85,80,90,80]}
+]
+
+console.log( Student[]+"student's name="+Student[].name)
+
+
 //avg for every element
-for(var i=0;i<Score.length;i++)
-{
-    var sum=0;
-    for(var j=0;j<Score[i].length;j++)
-    {
-        sum+=Score[i][j];
-    }
-    console.log("學生 "+(i+1)+" 分數平均值="+(sum/Score[i].length));
-}
+// for(var i=0;i<Score.length;i++)
+// {
+//     var sum=0;
+//     for(var j=0;j<Score[i].length;j++)
+//     {
+//         sum+=Score[i][j];
+//     }
+//     console.log("學生 "+(i+1)+" 分數平均值="+(sum/Score[i].length));
+// }
 

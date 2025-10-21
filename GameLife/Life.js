ac2802881c33f989c9a19ead64b0d16362026566
yun413 <wangyunting1024@gmail.com>
2025-10-21 16:05:18 +0800

    251021

---
+++ GameLife/Life.js
@@ -0,0 +1,23 @@
+const LIVE = 1, DEAD = 0
+
+class Life {
+    constructor(_row, _col) {
+        this.row = _row;
+        this.col = _col;
+        this.grid = []
+
+        //dynamic 2d array
+        // var aryRowCol=[];
+        // for (let _row = 0; _row < row; _row++) {
+        //     aryRowCol.push([]);
+        //     for (let _col = 0; _col < col; _col++) {
+        //         aryRowCol[r].push(_col)
+       
+        //     }
+        // }
+
+    }
+}
+
+var myGame1 = new Life(10,10)
+var myGame2 = new Life(5,5)

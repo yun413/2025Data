04ee7d66311d112bc61b471e633b6db185f20a7a
yun413 <wangyunting1024@gmail.com>
2025-12-23 16:10:58 +0800

    251223

--- swap.js
+++ swap.js
@@ -1,15 +1,15 @@
 //1.bubble sort
-function swap(a, b){
-    var temp=a;
-    a=b;
-    b=temp;
-    return;
-}
+// function swap(a, b){
+//     var temp=a;
+//     a=b;
+//     b=temp;
+//     return;
+// }
 
-var t=6, y=1;
-console.log("t="+t+", y="+y);
-swap(t,y);
-console.log("t="+t+", y="+y);
+// var t=6, y=1;
+// console.log("t="+t+", y="+y);
+// swap(t,y);
+// console.log("t="+t+", y="+y);
 
 
 //w15 bubble sort 要看懂程式碼
